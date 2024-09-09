97a374dc6209aef8c65aef364cf78c99fc73f268
Toohey, Hunter <hunter.toohey@nrel.gov>
2024-09-09 16:03:38 -0600

    tried just copying everything over

--- src/python/otsim/rpi_gpio/rpi_gpio.py
+++ src/python/otsim/rpi_gpio/rpi_gpio.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-import logging, signal, sys, threading, time, typing
+import json, logging, signal, sys, threading, typing
 
 import otsim.msgbus.envelope as envelope
 import xml.etree.ElementTree as ET
 
-from otsim.msgbus.envelope   import Envelope, Point
-from otsim.msgbus.pusher     import Pusher
+from otsim.msgbus.envelope   import Envelope
 from otsim.msgbus.subscriber import Subscriber
 
 import RPi.GPIO as GPIO
@@ -14,17 +13,11 @@ import RPi.GPIO as GPIO
 
 class RPiGPIO:
   def __init__(self: RPiGPIO, pub: str, pull: str, el: ET.Element):
-    # map pin numbers --> tag
-    self.inputs: typing.Dict[int, str] = {}
     # map tags --> pin number
     self.outputs: typing.Dict[str, int] = {}
 
-    self.monitoring = True
-
     self.name = el.get('name', default='ot-sim-rpi-gpio')
     mode      = el.get('mode', default='BOARD')
-
-    GPIO.setwarnings(False)
 
     if mode.upper() == 'BOARD':
       GPIO.setmode(GPIO.BOARD)
@@ -34,20 +27,8 @@ class RPiGPIO:
       print(f"unknown GPIO mode '{mode}' - defaulting to 'BOARD' mode")
       GPIO.setmode(GPIO.BOARD)
 
-    pub_endpoint  = el.findtext('pub-endpoint', default=pub)
-    pull_endpoint = el.findtext('pull-endpoint', default=pull)
-
+    pub_endpoint    = el.findtext('pub-endpoint', default=pub)
     self.subscriber = Subscriber(pub_endpoint)
-    self.pusher     = Pusher(pull_endpoint)
-
-    self.period = float(el.findtext('period', default=5))
-
-    for o in el.findall('input'):
-      pin = int(o.get('pin'))
-      tag = o.findtext('tag')
-
-      self.inputs[pin] = tag
-      GPIO.setup(pin, GPIO.IN)
 
     for o in el.findall('output'):
       pin = int(o.get('pin'))
@@ -62,34 +43,10 @@ class RPiGPIO:
   def start(self: RPiGPIO):
     self.subscriber.start('RUNTIME')
 
-    # run GPIO monitor in a thread
-    self.monitor_thread = threading.Thread(target=self.monitor, daemon=True)
-    self.monitor_thread.start()
-
 
   def stop(self: RPiGPIO):
-    self.monitoring = False
-    self.monitor_thread.join(self.period)
-
     GPIO.cleanup()
     self.subscriber.stop()
-
-
-  def monitor(self: RPiGPIO):
-    if len(self.inputs) == 0:
-      return
-
-    while self.monitoring:
-      points: typing.List[Point] = []
-
-      for pin, tag in self.inputs.items():
-        val = GPIO.input(pin)
-        points.append({'tag': tag, 'value': float(val), 'ts': 0})
-
-      env = envelope.new_status_envelope(self.name, {'measurements': points})
-      self.pusher.push('RUNTIME', env)
-
-      time.sleep(self.period)
 
 
   def handle_msgbus_update(self: RPiGPIO, env: Envelope):
